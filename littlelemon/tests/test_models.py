3d3b0a12bf14b3c14bd7f1e31fda2529e6e82342
Fam Awad <fam@awadlouis.com>
2024-02-21 17:04:27 +0200

    Adding unit testing:

--- littlelemon/tests/test_models.py
+++ littlelemon/tests/test_models.py
@@ -4,6 +4,6 @@ from restaurant.models import Menu
 class MenuItemTest(TestCase): 
   def test_get_item(self): 
     item = Menu.objects.create(title='Ice-Cream', price=80, inventory=100)
-    itemstr = item.get_item()
+    itemstr = item.__str__()
 
     self.assertEqual(itemstr, 'Ice-Cream : 80')
